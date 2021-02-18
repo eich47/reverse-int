73baf5efe62d075c929791a1b31c965249734fd5
eich47 <eich@tut.by>
2021-02-18 17:11:04 +0300

    feat: implement a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const revertStr = String(Math.abs(n)).split("").reverse().join("");
+    return Number(revertStr);
+};
